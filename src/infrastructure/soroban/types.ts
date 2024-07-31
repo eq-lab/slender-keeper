db5c57675028db2eae76fef3a88834322e248905
Maks Nabokov <max.nab@equilibrium.io>
2024-07-31 10:56:38 +0300

    fix reserve types

--- src/infrastructure/soroban/types.ts
+++ src/infrastructure/soroban/types.ts
@@ -3,8 +3,24 @@ export interface ReserveData {
     debt_token: string;
 }
 
+interface ReserveConfiguration {
+    is_active: boolean;
+    borrowing_enabled: boolean;
+    liquidity_cap: number;
+    pen_order: number;
+    util_cap: number;
+    discount: number;
+}
+
 export interface PoolReserveData {
-    reserve_type: string[]
+    reserve_type: string[];
+    configuration: ReserveConfiguration;
+    lender_ar: number;
+    lender_ir: number;
+    borrower_ar: number;
+    borrower_ir: number;
+    last_update_timestamp: number;
+    id: number[];
 }
 
 export interface PoolAccountPosition {
